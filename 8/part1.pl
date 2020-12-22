e85aed7b29d0b793fc951031a21d12a981001e5e
Raine M. Ekman <raine@iki.fi>
2020-12-22 09:04:45 +0200

    Slight cleanup and figured out that [] and () are very different when it comes to arrays.

--- 8/part1.pl
+++ 8/part1.pl
@@ -8,7 +8,7 @@ use strict;
 #}
 
 # Load program
-my @program = [];
+my @program = ();
 my $cmd;
 my $arg;
 while(<>) {
@@ -19,19 +19,19 @@ print(scalar @program);
 print "\n";
 
 #Run
-my $pc = 1; # For some reason the @program array starts from 1, thought it should be from 0
+my $pc = 0; 
 my $acc = 0;
 my $seen;
 while(1) {
-	print "${pc} : ${acc}\n";
 	my %cur = %{$program[$pc]};
 	if($cur{"seen"} != 0) {
-		print "Terminated " + $acc + "\n";
+		print $acc;
+		print "\n";
 		exit;
 	} else {
 		$program[$pc]{"seen"} = 1;
-		if   ($cur{"cmd"} eq "acc") { $acc += $cur{"arg"}; $pc = $pc + 1; print "+\n";}		
-		elsif($cur{"cmd"} eq "jmp") { $pc = $pc + ($cur{"arg"} + 0); print ">\n";}
-		elsif($cur{"cmd"} eq "nop") { $pc = $pc + 1; print ".\n";}
+		if   ($cur{"cmd"} eq "acc") { $acc += $cur{"arg"}; $pc = $pc + 1; }		
+		elsif($cur{"cmd"} eq "jmp") { $pc = $pc + ($cur{"arg"} + 0); }
+		elsif($cur{"cmd"} eq "nop") { $pc = $pc + 1; }
 	}
 }
